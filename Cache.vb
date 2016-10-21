652c184d0ded0471f39cdcba20bcb4c1f96eb10e
Olalekan Micheal <micheaola2102@gmail.com>
2016-10-22 03:16:01 +0530

    Update Cache.vb

--- Cache.vb
+++ Cache.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.SqlServerCe
 
 Module Cache
-    Dim con As SqlCeConnection = New SqlCeConnection("Data Source= |DataDirectory|\final.sdf;Password=ladybecky@2102")
+    Dim con As SqlCeConnection = New SqlCeConnection("Data Source= |DataDirectory|\final.sdf;Password=password")
 
 
 
